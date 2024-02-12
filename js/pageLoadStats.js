641634a3d2b924e124f24f4df86cb1b9dfbb7f74
turtle <super.ivannikov2013@yandex.ru>
2024-02-12 06:13:06 +0300

    lab5

--- js/pageLoadStats.js
+++ js/pageLoadStats.js
@@ -3,7 +3,7 @@ function measurePageLoadTime() {
     const loadingInfo = document.getElementById("loading-info");
 
     if (loadingInfo) {
-        loadingInfo.textContent = `The page is loaded in ${loadTime.toFixed(2)} milliseconds`;
+        loadingInfo.innerHTML = `The page loaded in <span class="load-time">${loadTime.toFixed(2)} </span> milliseconds`;
     } else {
         console.error("Element with id 'loading-info' not found");
     }
